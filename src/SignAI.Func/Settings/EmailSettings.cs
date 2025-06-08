094322a0f82d3ee3329708889b2cc55820d7b550
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2025-06-08 22:21:49 +0200

    update email settings

--- src/SignAI.Func/Settings/EmailSettings.cs
+++ src/SignAI.Func/Settings/EmailSettings.cs
@@ -6,7 +6,7 @@ public class EmailSettings
     public string DefaultSubject { get; set; } = "Your New Awesome Signature";
     public bool TrackOpens { get; set; } = true;
     public string DefaultTextBody { get; set; } = "Plain Text Body";
-    public string DefaultHtmlTemplate { get; set; } = "<html><body>This is your new email signature! <br /> {{signature}} </body></html>";
+    public string DefaultHtmlTemplate { get; set; } = "<html><body>This is your new email signature! <br /> <hr /> <br /> {{signature}} </body></html>";
     public string DefaultTag { get; set; } = "Your New Awesome Signature";
     public string DefaultMessageStream { get; set; } = "outbound";
     public bool EnableReplyTracking { get; set; } = true;
